04afc9434996cb4d3839a7c6894ed7a029893799
dragonpt <cesar.maximo@gmail.com>
2014-05-14 23:17:51 +0100

    first commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon May 5 11:02:02 CST 2014"
+#define LINUX_COMPILE_BY "mtk81316"
+#define LINUX_COMPILE_HOST "mtkswglt106"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ kernel/include/generated/utsrelease.h
@@ -0,0 +1,3 @@
+#define UTS_RELEASE "3.4.5"
+#define BUILD_INFO "ALPS.JB.SP.V1"
+#define BUILD_FINGERPRINT "lenovo89_cu_jb"
